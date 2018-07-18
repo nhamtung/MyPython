b8a9f61449a8865d8ba229cb2357b7d089356e11
nhamtung <tung.nham@ntq-solution.com.vn>
2018-07-18 11:43:31 +0700

    AwsIotPubSub

---
+++ AWSIoTPythonSDK/TungNV/AwsIotPubSub/AwsIotPub.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+############################################
+# PROBLEM STATEMENT:
+# This program will publish test mqtt messages using the AWS IOT hub
+#
+# To test this program you have to run first its companinon aws_iot_sub.py
+# that will subscribe and show all the messages sent by this program
+#
+############################################
+
+############################################
+# STEPS:
+#
+# 1. Sign in to AWS Amazon > Services > AWS IoT > Settings > copy Endpoint
+#    This is your awshost
+#
+# 2. Change following things in the below program:
+#    a. awshost   (from step 1)
+#    b. clientId  (Thing_Name)
+#    c. thingName (Thing_Name)
+#    d. caPath    (root-CA_certificate_Name)
+#    e. certPath  (<Thing_Name>.cert.pem)
+#    f. keyPath   (<Thing_Name>.private.key)
+#
+# 3. Paste aws_iot_pub.py & aws_iot_sub.py python scripts in folder where all unzipped aws files are kept.
+# 4. Provide Executable permition for both the python scripts.
+# 5. Run aws_iot_sub.py script
+# 6. Run this aws_iot_pub.py python script
+#
+# http://www.apdaga.com/2018/02/install-aws-amazon-sdk-on-raspberry-pi.html
+############################################
+
+# importing libraries
+import paho.mqtt.client as paho
+import os
+import socket
+import ssl
+from time import sleep
+from random import uniform
+
+connflag = False
+
+def on_connect(client, userdata, flags, rc):  # func for making connection
+    global connflag
+    print("Connected to AWS")
+    connflag = True
+    print("Connection returned result: " + str(rc))
+
+def on_message(client, userdata, msg):  # Func for Sending msg
+    print(msg.topic + " " + str(msg.payload))
+
+# def on_log(client, userdata, level, buf):
+#    print(msg.topic+" "+str(msg.payload))
+
+mqttc = paho.Client()  # mqttc object
+mqttc.on_connect = on_connect  # assign on_connect func
+mqttc.on_message = on_message  # assign on_message func
+# mqttc.on_log = on_log
+
+
+awshost = "avt6g3kvwjn2o.iot.ap-southeast-1.amazonaws.com"  # Endpoint
+awsport = 443  # Port no.
+clientId = "BasicPubSubPython"  # Thing_Name
+thingName = "BasicPubSubPython"  # Thing_Name
+caPath = "D:\MyFolder\TUNG\MyPython\AWSIoTPythonSDK\TungNV\Certificate\BasicPubSubPython\\rootCA.pem"  # Root_CA_Certificate_Name
+certPath = "D:\MyFolder\TUNG\MyPython\AWSIoTPythonSDK\TungNV\Certificate\BasicPubSubPython\\44432477c8-certificate.pem"  # <Thing_Name>.cert.pem
+keyPath = "D:\MyFolder\TUNG\MyPython\AWSIoTPythonSDK\TungNV\Certificate\BasicPubSubPython\\44432477c8-private.pem.key"  # <Thing_Name>.private.key
+
+print("awshost = ", awshost)
+print("awsport = ", awsport)
+print("clientId = ", clientId)
+print("thingName = ", thingName)
+print("caPath = ", caPath)
+print("certPath = ", certPath)
+print("keyPath = ", keyPath)
+
+mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)  # pass parameters
+mqttc.connect(awshost, awsport, keepalive=60)  # connect to aws server
+mqttc.loop_start()  # Start the loop
+
+# while 1 == 1:
+#     sleep(5)
+#     if connflag == True:
+#         tempreading = uniform(20.0, 25.0)  # Generating Temperature Readings
+#         mqttc.publish("temperature", tempreading, qos=1)  # topic: temperature # Publishing Temperature values
+#         print("msg sent: temperature " + "%.2f" % tempreading)  # Print sent temperature msg on console
+#     else:
+#         print("waiting for connection...")
